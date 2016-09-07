fc730960ac6314760cfd69adf9f013658e6224ce
zxth93 <zhangruiscu@foxmail.com>
2016-09-07 19:34:13 +0800

    reverse-string

--- ReverseString.h
+++ ReverseString.h
@@ -8,6 +8,15 @@
 
 #ifndef ReverseString_h
 #define ReverseString_h
-
+class Solution {
+public:
+    std::string reverseString(std::string s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            std::swap(s[i], s[j]);
+        }
+        
+        return s;
+    }
+};
 
 #endif /* ReverseString_h */
