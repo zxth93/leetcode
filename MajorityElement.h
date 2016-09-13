5007d7ff4ff5f37f72e40ee79d0bc42fe78b62ec
zxth93 <zhangruiscu@foxmail.com>
2016-09-13 19:36:48 +0800

    169 majority element

---
+++ MajorityElement.h
@@ -0,0 +1,18 @@
+//
+//  MajorityElement.h
+//  leetcode
+//
+//  Created by R Z on 16/9/12.
+//  Copyright © 2016年 R Z. All rights reserved.
+//
+
+#ifndef MajorityElement_h
+#define MajorityElement_h
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+    }
+};
+
+#endif /* MajorityElement_h */
