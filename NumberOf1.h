2456fd2a6f92a0b4c3acf8eefc7763d68fe091b3
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:44:37 +0800

    number of 1

--- NumberOf1.h
+++ NumberOf1.h
@@ -8,6 +8,19 @@
 
 #ifndef NumberOf1_h
 #define NumberOf1_h
-
+using namespace std;
+class Solution{
+    public:
+        int hammingWeight(uint32_t n){
+            int count=0;
+            while(n>0){
+                if(n & 1){
+                    count++;
+                }
+                n=n>>1;
+            }
+            return count;
+        }
+};
 
 #endif /* NumberOf1_h */
