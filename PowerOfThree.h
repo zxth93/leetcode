b98e97a91c65a628115545f92c3502ee20665431
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:47:29 +0800

    power of three

--- PowerOfThree.h
+++ PowerOfThree.h
@@ -8,6 +8,29 @@
 
 #ifndef PowerOfThree_h
 #define PowerOfThree_h
-
+#include <string>
+using namespace std;
+class Solution{
+    public:
+        bool isPowerOfThree(int n){
+            if(n==1){
+                return true;
+            }
+            if(n<0){
+                return false;
+            }
+            while(n != 0){
+                if(n%3==0){
+                    n=n/3;
+                    if(n==1){
+                        return true;
+                    }
+                }else{
+                    break;
+                }
+            }
+            return false;
+        }
+};
 
 #endif /* PowerOfThree_h */
