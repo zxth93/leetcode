40404a1a21fe0ec253d58696bf0f44d1c61048c3
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:35:14 +0800

    448

--- 448_	Find_All_Numbers_Disappeared_in_an_Array.cpp
+++ 448_	Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -7,3 +7,25 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> result;
+        vector<int> extra(nums.size(), 0);
+        for(int i = 0 ; i<nums.size(); i++){
+            extra[nums[i]-1]++;
+        }
+        for(int i = 0; i<nums.size(); i++){
+            if(extra[i]==0){
+                result.push_back(i+1);
+            }
+        }
+        
+        return result;
+    }
+};
