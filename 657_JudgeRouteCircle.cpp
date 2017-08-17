4dad060f766f82c6f9f79d37b75af92981291763
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:38:29 +0800

    657

--- 657_JudgeRouteCircle.cpp
+++ 657_JudgeRouteCircle.cpp
@@ -7,3 +7,29 @@
 //
 
 #include <stdio.h>
+#include <iosteam>
+using namespace std;
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int position[] = {0, 0};
+        int i = 0;
+        while(moves[i]!='\0'){
+            if(moves[i] == 'U'){
+                position[0]++;
+            }else if(moves[i] == 'D'){
+                position[0]--;
+            }else if(moves[i] == 'L'){
+                position[1]--;
+            }else if(moves[i] == 'R'){
+                position[1]++;
+            }
+            i++;
+        }
+        
+        if(position[0] == 0 && position[1] == 0){
+            return true;
+        }
+        return false;
+    }
+};
