6e4c5f4595020a2a73b99bc95f26dbf5d05e09c6
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:39:50 +0800

    climbingstairs

--- ClimbingStairs.h
+++ ClimbingStairs.h
@@ -8,6 +8,25 @@
 
 #ifndef ClimbingStairs_h
 #define ClimbingStairs_h
+class Solution{
+    public:
+        int data[100]={0};
+        int climbStairs(int n){
+            if(n==1 || n==0){
+                return 1;
+            }
+            if(!containNum(n)){
+                data[n-1]=climbStairs(n-1)+climbStairs(n-2);
+            }
+            return data[n-1];
+        }
 
+        bool containNum(int n){
+            if(data[n-1]!=0){
+                return true;
+            }
+            return false;
+        }
+}
 
 #endif /* ClimbingStairs_h */
