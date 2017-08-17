54021eddd14302408d0e80b44d593ddcbcd889ca
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:36:27 +0800

    561

--- 561_ArrayPartition1.cpp
+++ 561_ArrayPartition1.cpp
@@ -7,3 +7,22 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for(int i =0; i<nums.size();i+=2){
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
