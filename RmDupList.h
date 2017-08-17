2e265693b7d342b1d2a150b716087006a92fbf0d
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:49:47 +0800

    rm dup list

--- RmDupList.h
+++ RmDupList.h
@@ -5,9 +5,33 @@
 //  Created by R Z on 16/9/29.
 //  Copyright © 2016年 R Z. All rights reserved.
 //
-
+/**
+ * struct ListNode{
+ *  int val;
+ *  ListNode* next;
+ *  ListNode(int x): val(x),next(null)
+ *
+ * }
+*/
 #ifndef RmDupList_h
 #define RmDupList_h
-
+class Solution{
+    public:
+        ListNode* deleteDuplicates(ListNode* head){
+            if(head==NULL||head->next==NULL){
+                return head;
+            }
+            ListNode* temp=head;
+            while(temp->next!=NULL){
+                if(temp->val==temp->next->val){
+                    temp->next=temp->next->next;
+                    //temp->next->next=NULL;
+                    continue;
+                }
+                temp=temp->next;
+            }
+            return head;
+        }
+};
 
 #endif /* RmDupList_h */
