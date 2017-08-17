46985af39ff20a0375f4272e039ba9b6a1f16d93
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:39:03 +0800

    66

--- 66_PlusOne.cpp
+++ 66_PlusOne.cpp
@@ -7,3 +7,60 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        if(digits.size() == 0){
+            return digits;
+        }
+        
+        int digit_end = digits.back();
+        if(digit_end < 9){
+            digit_end++;
+            digits.pop_back();
+            digits.push_back(digit_end);
+            return digits;
+        }
+        
+        int count = digits.size();
+        
+        bool flag = true;
+        while(count>0){
+            int temp_end = digits.at(count-1);
+            if(flag){
+                if(temp_end ==9){
+                    digits.erase(digits.begin()+count-1);
+                    digits.insert(digits.begin()+count-1, 0);
+                }else{
+                    digits.erase(digits.begin()+count-1);
+                    digits.insert(digits.begin()+count-1, temp_end+1);
+                    flag = false;
+                    break;
+                }
+            }else{
+                if(temp_end ==9){
+                    digits.erase(digits.begin()+count-1);
+                    digits.insert(digits.begin()+count-1, 0);
+                    flag = true;
+                }else{
+                    digits.erase(digits.begin()+count-1);
+                    digits.insert(digits.begin()+count-1, temp_end+1);
+                }
+            }
+            count--;
+        }
+        if(flag){
+            digits.insert(digits.begin(), 1);
+        }
+        
+        return digits;
+        
+    }
+};
