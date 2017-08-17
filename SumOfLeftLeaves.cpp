6db07bd7f4b7e132405bfe3f7e8936f2ce42d46c
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:50:38 +0800

    sum of left leaves

--- SumOfLeftLeaves.cpp
+++ SumOfLeftLeaves.cpp
@@ -5,5 +5,32 @@
 //  Created by R Z on 16/10/11.
 //  Copyright © 2016年 R Z. All rights reserved.
 //
-
+/**
+ * struct TreeNode{
+ *      int val;
+ *      TreeNode* left;
+ *      TreeNode* right;
+ *      TreeNode(int x) : val(x), lefet(NULL),right(NULL){}
+ * };
+ */
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+/*class Solution{
+    public:
+        int sumOfLeftLeaves(TreeNode* root){
+            if(root == NULL){
+                return 0;
+            }
+
+            if(root->left == NULL && root->right == NULL){
+                return root->val;
+            }
+
+            int result = 0;
+            result += sumOfLeftLeaves(root->left);
+            result += SumOfLeftLeaves(root->right);
+            return result;
+        }
+};*/
