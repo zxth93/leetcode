91dfddf1a9672dd1884863aebbcf02d1a6c75952
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:32:14 +0800

    1

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -7,3 +7,27 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int i,j;
+        
+        for(i=0; i<nums.size()-1; i++){
+            for(j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
