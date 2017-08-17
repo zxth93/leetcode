b26fd3cbd122831a0ad893ee5601c57685b59847
zxth93 <zhangruiscu@foxmail.com>
2017-08-17 20:48:53 +0800

    power of two

--- PowerOfTwo.h
+++ PowerOfTwo.h
@@ -8,6 +8,24 @@
 
 #ifndef PowerOfTwo_h
 #define PowerOfTwo_h
-
+using namespace std;
+class Solution{
+public:
+    bool isPowerOfTwo(int n){
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+            n>>=1;
+            if(n==1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
 
 #endif /* PowerOfTwo_h */
