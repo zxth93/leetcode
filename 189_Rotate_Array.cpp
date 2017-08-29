a3e171890ff454529f01f58f3f0dcf663cf9e14b
zxth93 <zhangruiscu@foxmail.com>
2017-08-29 20:21:14 +0800

    189 array

--- 189_Rotate_Array.cpp
+++ 189_Rotate_Array.cpp
@@ -7,3 +7,18 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i=0; i<k;i++){
+            int temp = nums[n-1];
+            nums.pop_back();
+            nums.insert(nums.begin(), temp);
+        }
+    }
+};
