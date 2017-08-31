4e107e8f1c56451a5834cc050a2401fc041d1290
zxth93 <zhangruiscu@foxmail.com>
2017-08-31 20:13:10 +0800

    35 array

--- 35_Search_Insert_Position.cpp
+++ 35_Search_Insert_Position.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        for(int i=0; i<nums.size(); i++){
+            if(i==0 && nums[0] > target){
+                return 0;
+            }
+            if(nums[i] - target == 0){
+                return i;
+            }
+            if(nums[i] > target){
+                return i;
+            }
+        }
+        
+        return nums.size();
+    }
+};
