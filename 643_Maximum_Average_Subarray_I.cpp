66f93bb8b2e67200e80617abf663663eea4f9315
zxth93 <zhangruiscu@foxmail.com>
2017-09-08 20:10:16 +0800

    643 array

--- 643_Maximum_Average_Subarray_I.cpp
+++ 643_Maximum_Average_Subarray_I.cpp
@@ -7,3 +7,40 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0;
+        int max = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i<k){
+                sum += nums[i];
+                max = sum;
+            }else{
+                sum = sum+nums[i]-nums[i-k];
+                max = max > sum ? max : sum;
+            }
+            
+        }
+        
+        return max/(float)k;
+    }
+    
+    double findMaxAverage2(vector<int>& nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) sum += nums[i];
+        int max = sum;
+        
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k];
+            max = max > sum ? max : sum;
+        }
+        
+        return max / 1.0 / k;
+    }
+};
