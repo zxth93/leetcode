f11dec9c1560dc504eb4eacac92aabad84d099d1
zxth93 <zhangruiscu@foxmail.com>
2017-09-11 10:14:42 +0800

    628 array

--- 628_Maximum_Product_of_Three_Numbers.cpp
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -7,3 +7,19 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int len = nums.size();
+        int temp1 = nums[len-1]*nums[0]*nums[1];
+        
+        int temp2 = nums[len-1]*nums[len-2]*nums[len-3];
+        
+        return temp1>temp2?temp1:temp2;
+    }
+};
