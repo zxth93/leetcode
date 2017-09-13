328b40ba82e321f94c009b45f0e035fef253f9dc
zxth93 <zhangruiscu@foxmail.com>
2017-09-13 20:06:06 +0800

    566 array

--- 566. Reshape the Matrix.cpp
+++ 566. Reshape the Matrix.cpp
@@ -7,3 +7,29 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int row = nums.size();
+        int col = nums[0].size();
+        if(row*col != r*c){
+            return nums;
+        }
+        
+        vector<vector<int>> result(r);
+        for(int i=0; i<r;i++){
+            result[i].resize(c);
+        }
+        
+        for(int i=0; i<row*col; i++){
+            result[i/c][i%c] = nums[i/col][i%col];
+        }
+        //delete result;
+        return result;
+    }
+};
