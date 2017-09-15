703905af3c55905f0ad0b26ad25396ca255d33e5
zxth93 <zhangruiscu@foxmail.com>
2017-09-15 16:08:53 +0800

    605 array

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -7,3 +7,24 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for(int i=0; i<flowerbed.size(); i++){
+            if(flowerbed[i] == 0){
+                int next = (i==flowerbed.size()-1) ? 0 : flowerbed[i+1];
+                int pre = (i==0) ? 0 : flowerbed[i-1];
+                if(next == 0 && pre == 0){
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+        return count>=n;
+    }
+};
---
+++ 67. Add Binary.cpp
@@ -0,0 +1,9 @@
+//
+//  67. Add Binary.cpp
+//  leetcode
+//
+//  Created by R Z on 2017/9/13.
+//  Copyright © 2017年 R Z. All rights reserved.
+//
+
+#include <stdio.h>
