c91c0d477cba25ecbb0f9d42e9b3eb7c410fa972
zxth93 <zhangruiscu@foxmail.com>
2017-09-22 22:16:30 +0800

    345 string

--- 345. Reverse Vowels of a String.cpp
+++ 345. Reverse Vowels of a String.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        stack<char> bag;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U') bag.push(s[i]);
+            
+        }
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                s[i]=bag.top();
+                bag.pop();
+            }
+            
+        }
+        return s;
+    }
+};
