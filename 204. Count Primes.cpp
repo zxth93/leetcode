fac73965c831d24c9afa23fae1af668fd869a418
zxth93 <zhangruiscu@foxmail.com>
2017-09-23 20:45:31 +0800

    434 string

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,9 @@
+//
+//  204. Count Primes.cpp
+//  leetcode
+//
+//  Created by R Z on 2017/9/23.
+//  Copyright © 2017年 R Z. All rights reserved.
+//
+
+#include <stdio.h>
--- 434. Number of Segments in a String.cpp
+++ 434. Number of Segments in a String.cpp
@@ -7,3 +7,24 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int countSegments(string s) {
+        if(s.size() < 1) return 0;
+        int count =0;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ' && (i==0 ||s[i-1] ==' ')){
+                count++;
+            }
+            //if(s[i]=='\0' && count > 0) count++;
+        }
+        
+        return count;
+    }
+};
