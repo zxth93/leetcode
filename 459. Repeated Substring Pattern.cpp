1731fd2877874a3e09f7bf86b81ab5ead89d0fa1
zxth93 <zhangruiscu@foxmail.com>
2017-09-25 22:19:18 +0800

    459 string

--- 459. Repeated Substring Pattern.cpp
+++ 459. Repeated Substring Pattern.cpp
@@ -7,3 +7,16 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        if(s.size() < 2) return false;
+        string temp = s+s;
+        temp = temp.substr(1,temp.size()-2);
+        return temp.find(s)!=-1;
+    }
+};
