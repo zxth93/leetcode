2f4d46b1734dc7c0dccf5f1e8f36a1f160717455
zxth93 <zhangruiscu@foxmail.com>
2017-09-26 19:37:35 +0800

    521 string

--- 521. Longest Uncommon Subsequence I.cpp
+++ 521. Longest Uncommon Subsequence I.cpp
@@ -7,3 +7,15 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        if(a==b) return -1;
+        return a.size()>b.size()?a.size():b.size();
+    }
+};
