4221a4ec6cc2a9d7cbf885b3739ab7add8e3306c
zxth93 <zhangruiscu@foxmail.com>
2017-09-30 21:58:07 +0800

    541 string

---
+++ 501. Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,9 @@
+//
+//  501. Find Mode in Binary Search Tree.cpp
+//  leetcode
+//
+//  Created by R Z on 2017/9/28.
+//  Copyright © 2017年 R Z. All rights reserved.
+//
+
+#include <stdio.h>
--- 541. Reverse String II.cpp
+++ 541. Reverse String II.cpp
@@ -7,3 +7,39 @@
 //
 
 #include <stdio.h>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int count = 0;
+        stack<char> st;
+        
+        for(int i=0; i<s.size(); i++){
+            
+            if(count == k){
+                for(int j=k; j>0; j--){
+                    s[i-j] = st.top();
+                    st.pop();
+                }
+                //flag = false;
+                i += k-1;
+                count = 0;
+            }else{
+                st.push(s[i]);
+                count++;
+            }
+            
+        }
+        if(count != 0){
+            for(int j=count; j>0; j--){
+                s[s.size()-j] = st.top();
+                st.pop();
+            }
+        }
+        return s;
+    }
+};
