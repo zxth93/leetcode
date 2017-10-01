42b3263ea79b37dc2f52e59922119d19195f58e0
zxth93 <zhangruiscu@foxmail.com>
2017-10-01 08:14:01 +0800

    606 string

--- 606. Construct String from Binary Tree.cpp
+++ 606. Construct String from Binary Tree.cpp
@@ -7,3 +7,34 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        string result = "";
+        if(t == NULL){
+            return "";
+        }
+        result += to_string(t->val);
+        string left = tree2str(t->left);
+        string right = tree2str(t->right);
+        
+        if(left==""&&right=="") return result;
+        if(left=="") return result+"()"+"("+right+")";
+        if(right=="") return result +"("+left+")";
+        
+        return result + "(" + left + ")" + "(" +right + ")";
+    }
+};
