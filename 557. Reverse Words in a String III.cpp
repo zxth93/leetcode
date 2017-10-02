5b93c52e519b0234f679322c6006cb0c10c6de7a
zxth93 <zhangruiscu@foxmail.com>
2017-10-02 17:36:44 +0800

    557 string

--- 557. Reverse Words in a String III.cpp
+++ 557. Reverse Words in a String III.cpp
@@ -7,3 +7,30 @@
 //
 
 #include <stdio.h>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int count = 0;
+        stack<char> st;
+        for(int i=0; i<=s.size(); i++){
+            
+            if(s[i] == ' ' || s[i] == '\0'){
+                for(int j=count; j>0; j--){
+                    s[i-j]=st.top();
+                    st.pop();
+                }
+                count = 0;
+            }else{
+                count++;
+                st.push(s[i]);
+            }
+            
+        }
+        return s;
+    }
+};
