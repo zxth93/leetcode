ecb14244eed41887a5d291ced6dabc14285b1cfc
zxth93 <zhangruiscu@foxmail.com>
2017-10-03 21:24:47 +0800

    551 string

--- 551. Student Attendance Record I.cpp
+++ 551. Student Attendance Record I.cpp
@@ -7,3 +7,34 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        if(s.size() < 1){
+            return false;
+        }
+        
+        int a = 0;
+        int l = 0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == 'A'){
+                a++;
+            }
+            if(s[i] == 'L'){
+                l++;
+            }else{
+                l=0;
+            }
+            if(a > 1 || l >2){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
