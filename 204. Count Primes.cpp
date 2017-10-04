2bbd1c015898b32704074ec7a1cb2cb27be2abe1
zxth93 <zhangruiscu@foxmail.com>
2017-10-04 23:30:50 +0800

    204 math

--- 204. Count Primes.cpp
+++ 204. Count Primes.cpp
@@ -7,3 +7,25 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> check(n);
+        //bool []check = new bool[n];
+        int count = 0;
+        for(int i=2; i<n; i++){
+            if(check[i]==false){
+                count++;
+                for(int j=2;i*j<n;j++){
+                    check[j*i]=true;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
