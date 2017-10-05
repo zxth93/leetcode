f735e85fc19acabcdcb69e9d6e58dcc7ca5bce7c
zxth93 <zhangruiscu@foxmail.com>
2017-10-05 21:08:54 +0800

    263 math

--- 263. Ugly Number.cpp
+++ 263. Ugly Number.cpp
@@ -7,3 +7,23 @@
 //
 
 #include <stdio.h>
+
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        while(num > 1){
+            if(num%2 == 0){
+                num /= 2;
+            }else if(num%3 == 0){
+                num /= 3;
+            }else if(num%5 == 0){
+                num /= 5;
+            }else{
+                break;
+            }
+        }
+        
+        return num==1;
+    }
+};
