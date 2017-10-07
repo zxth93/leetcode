f3bda7501a30728b481ea637547e5b01c77c3fd1
zxth93 <zhangruiscu@foxmail.com>
2017-10-07 09:01:00 +0800

    400 math

--- 400. Nth Digit.cpp
+++ 400. Nth Digit.cpp
@@ -7,3 +7,35 @@
 //
 
 #include <stdio.h>
+
+
+class Solution {
+public:
+    int findNthDigit(int n) {
+        
+        long base = 9;
+        long digits = 1;
+        while(n-base*digits > 0){
+            n -= base*digits;
+            base *=10;
+            digits++;
+        }
+        
+        int index = n%digits;
+        if(index == 0){
+            index = digits;
+        }
+        int num = 1;
+        for(int i=1; i<digits; i++){
+            num *= 10;
+        }
+        
+        num += (index==digits) ? n/digits - 1 : n/digits;
+        for(int i=index; i<digits; i++){
+            num /= 10;
+        }
+        
+        return num%10;
+        
+    }
+};
