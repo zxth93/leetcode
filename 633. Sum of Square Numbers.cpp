e8b7a27e1220296f328b9efe41ec9d87b5381c3a
zxth93 <zhangruiscu@foxmail.com>
2017-10-08 18:24:38 +0800

    633 math

--- 633. Sum of Square Numbers.cpp
+++ 633. Sum of Square Numbers.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if(c < 0) return false;
+        int l=0;
+        int r=(int)sqrt(c);
+        while(l<=r){
+            int temp = l*l + r*r;
+            if(temp<c){
+                l++;
+            }else if(temp > c){
+                r--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
