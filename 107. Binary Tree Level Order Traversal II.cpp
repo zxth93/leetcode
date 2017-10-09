74b447553f2e3d1c34a55d32d8aab15bf5d8d4de
zxth93 <zhangruiscu@foxmail.com>
2017-10-09 22:11:11 +0800

    107 tree

--- 107. Binary Tree Level Order Traversal II.cpp
+++ 107. Binary Tree Level Order Traversal II.cpp
@@ -7,3 +7,41 @@
 //
 
 #include <stdio.h>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        stack<vector<int>> tower;
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        while(!q.empty()){
+            vector<int> temp;
+            int s = q.size();
+            for(int i=0; i<s; i++){
+                TreeNode* t = q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+                temp.push_back(t->val);
+            }
+            tower.push(temp);
+        }
+        while(!tower.empty()){
+            res.push_back(tower.top());
+            tower.pop();
+        }
+        return res;
+    }
+};
