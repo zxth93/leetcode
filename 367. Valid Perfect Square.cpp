5dde8f8d12703ae14e3669423f8ad5496ec78926
zxth93 <zhangruiscu@foxmail.com>
2017-10-18 22:00:42 +0800

    367 math

--- 367. Valid Perfect Square.cpp
+++ 367. Valid Perfect Square.cpp
@@ -7,3 +7,30 @@
 //
 
 #include <stdio.h>
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(int i=1; num>0; i+=2){
+            num -= i;
+        }
+        return num==0;
+    }
+    
+    bool isPerfectSquare2(int num){
+        int left = 1;
+        int right = num;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            int temp = mid*mid;
+            if(temp < num){
+                left=mid+1;
+            }else if(temp > num){
+                right = mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
