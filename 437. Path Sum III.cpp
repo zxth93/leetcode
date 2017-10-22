61cbc55f1c047fe94b640402bd920026ff9f0a08
zxth93 <zhangruiscu@foxmail.com>
2017-10-22 20:11:20 +0800

    437 tree

--- 437. Path Sum III.cpp
+++ 437. Path Sum III.cpp
@@ -7,3 +7,25 @@
 //
 
 #include <stdio.h>
+
+/**
+ * Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return pathSumFrom(root, sum) + pathSum(root->left,sum) + pathSum(root->right,sum);
+    }
+    
+    int pathSumFrom(TreeNode* root, int sum){
+        if(!root) return 0;
+        return (root->val==sum ? 1 : 0) + pathSumFrom(root->left, sum - root->val) + pathSumFrom(root->right,sum-root->val);
+    }
+};
