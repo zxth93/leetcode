28555fca75837f4705f71d1d4259bf9a78aa5977
zxth93 <zhangruiscu@foxmail.com>
2017-10-23 21:28:05 +0800

    112 tree

--- 112. Path Sum.cpp
+++ 112. Path Sum.cpp
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+/**
+ * Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root) return false;
+        if(root->val == sum && root->left == NULL && root->right==NULL) return true;
+        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
