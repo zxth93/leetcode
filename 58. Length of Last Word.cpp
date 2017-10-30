d6352eb204f07cbf273f65db3fedc936c5e50ee5
zxth93 <zhangruiscu@foxmail.com>
2017-10-30 22:09:20 +0800

    58 string

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -7,3 +7,27 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                count++;
+            }else{
+                if(s[i+1]=='\0'){
+                    break;
+                }else if(s[i+1]!=' '){
+                    count=0;
+                }
+            }
+            
+        }
+        return count;
+    }
+};
