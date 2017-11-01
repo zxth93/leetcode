fb33ef0b82728d9f86410282df7314bcf5d1eafd
zxth93 <zhangruiscu@foxmail.com>
2017-11-01 22:02:00 +0800

    653 tree

--- 653. Two Sum IV - Input is a BST.cpp
+++ 653. Two Sum IV - Input is a BST.cpp
@@ -7,3 +7,43 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+    vector<int> res;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        travel(root);
+        int i=0;
+        int j=res.size()-1;
+        while(i<j){
+            int sum = res[i]+res[j];
+            if(sum > k){
+                j--;
+            }else if(sum < k){
+                i++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void travel(TreeNode* root){
+        if(root == NULL) return;
+        travel(root->left);
+        res.push_back(root->val);
+        travel(root->right);
+        
+    }
+    
+};
