35396085f17f1fb5ece61ae33ac035346d461bce
zxth93 <zhangruiscu@foxmail.com>
2017-11-07 22:35:03 +0800

    671 tree

--- 671. Second Minimum Node In a Binary Tree.cpp
+++ 671. Second Minimum Node In a Binary Tree.cpp
@@ -7,3 +7,41 @@
 //
 
 #include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        
+        if(root->left){
+            if(root->left->val < root->right->val){
+                int l = findSecondMinimumValue(root->left);
+                if(l==-1) return root->right->val;
+                return min(l, root->right->val);
+            }else if(root->left->val > root->right->val){
+                int r = findSecondMinimumValue(root->right);
+                if(r==-1) return root->left->val;
+                return min(r, root->left->val);
+            }else{
+                int l = findSecondMinimumValue(root->left);
+                int r = findSecondMinimumValue(root->right);
+                if(l==-1) return r;
+                if(r==-1) return l;
+                return min(l, r);
+            }
+        }else{
+            return -1;
+        }
+        
+        return -1;
+    }
+};
