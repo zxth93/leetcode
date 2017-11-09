80e1dd2dd1ec6740b5d7824755fa3f251e7d0969
zxth93 <zhangruiscu@foxmail.com>
2017-11-09 22:38:11 +0800

    598 tree

--- 598. Range Addition II.cpp
+++ 598. Range Addition II.cpp
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        if(ops.size()<1) return m*n;
+        int row = ops[0][0];
+        int col = ops[0][1];
+        for(int i=0; i<ops.size();i++){
+            if(row > ops[i][0]) row = ops[i][0];
+            if(col > ops[i][1]) col = ops[i][1];
+        }
+        return row*col;
+    }
+};
