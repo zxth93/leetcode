1bd2c3a4725763643f365068819618b4bf0d16b8
zxth93 <zhangruiscu@foxmail.com>
2017-11-16 22:44:05 +0800

    645 string

--- 645. Set Mismatch.cpp
+++ 645. Set Mismatch.cpp
@@ -7,3 +7,47 @@
 //
 
 #include <stdio.h>
+#include <vector>
+#include <unordered_map>
+#include <math>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> hash;
+        vector<int> result;
+        for(int i=0; i<n; i++){
+            if(hash.count(nums[i])){
+                hash[nums[i]] += 1;
+            }else{
+                hash[nums[i]] = 1;
+            }
+        }
+        int end=0;
+        int dul=0;
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 2){
+                dul=i;
+            }
+            if(!hash[i]) end=i;
+        }
+        result.push_back(dul);
+        result.push_back(end);
+        return result;
+    }
+    
+    vector<int> findErrorNums2(vector<int>& nums){
+        int[] res = new int[2];
+        for (int i : nums) {
+            if (nums[Math.abs(i) - 1] < 0) res[0] = Math.abs(i);
+            else nums[Math.abs(i) - 1] *= -1;
+        }
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] > 0) res[1] = i+1;
+        }
+        return res;
+    }
+};
