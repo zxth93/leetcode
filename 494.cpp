e3bd406ac4f60bb08066fcd86896524114184550
zxth93 <zhangruiscu@foxmail.com>
2017-12-28 11:00:25 +0800

    494 dp

--- 494.cpp
+++ 494.cpp
@@ -7,3 +7,21 @@
 //
 
 #include <stdio.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum=0;
+        for(int num : nums) sum+=num;
+        return sum<S||(S+sum)%2>0?0:subsetSum(nums, (S+sum)>>1);
+    }
+    int subsetSum(vector<int> &nums, int target){
+        vector<int> dp(target+1, 0);
+        dp[0]=1;
+        for(int num : nums){
+            for(int i=target; i>=num; i--) dp[i]+=dp[i-num];
+        }
+        return dp[target];
+    }
+};
