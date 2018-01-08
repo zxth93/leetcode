adb43fc97dda9fd74ac46f3fc6ff53e9301af2f8
zxth93 <zhangruiscu@foxmail.com>
2018-01-08 22:15:51 +0800

    63 dp

--- 63.cpp
+++ 63.cpp
@@ -32,4 +32,18 @@ public:
         }
         return dp[m-1][n-1];
     }
+    
+    int uniquePathsWithObstacles2(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size(), n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        vector<int> dp(n,0);
+        dp[0]=1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j]==1) dp[j]=0;
+                else if(j>0) dp[j]+=dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
 };
