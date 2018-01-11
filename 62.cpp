a8d65bed46a49182151f33d09ae3dd714a1354e6
zxth93 <zhangruiscu@foxmail.com>
2018-01-11 21:45:47 +0800

    62 dp

--- 62.cpp
+++ 62.cpp
@@ -23,4 +23,15 @@ public:
         }
         return dp[m-1][n-1];
     }
+    
+    int uniquePaths2(int m, int n) {
+        vector<int> dp(n,0);
+        dp[0]=1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[j]+=dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
 };
