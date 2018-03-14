1505b13ffd64872464f751ac9391dc0c1282f47c
zxth93 <zhangruiscu@foxmail.com>
2018-03-14 21:12:24 +0800

    260 bit

---
+++ 260.cpp
@@ -0,0 +1,37 @@
+//
+//  260.cpp
+//  leetcode
+//
+//  Created by R Z on 2018/3/14.
+//  Copyright © 2018年 R Z. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        // Pass 1 :
+        // Get the XOR of the two numbers we need to find
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        // Get its last set bit
+        diff &= -diff;
+        
+        // Pass 2 :
+        vector<int> rets = {0, 0}; // this vector stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
+    }
+    
+};
