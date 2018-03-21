7e3676f1b961ec09fd5de552169b9a8883240209
zxth93 <zhangruiscu@foxmail.com>
2018-03-21 20:38:44 +0800

    73 array

--- 73.cpp
+++ 73.cpp
@@ -27,4 +27,22 @@ public:
             for(int m=0;m<rows;m++) matrix[m][j]=0;
         }
     }
+    
+    void setZeroes2(vector<vector<int> > &matrix) {
+        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+        
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+        
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }
 };
