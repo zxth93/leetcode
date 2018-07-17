e27a87c46d4296bfdf2e094d3636fc53f8e7fd1e
zxth93 <zhangruiscu@foxmail.com>
2018-07-17 22:22:41 +0800

    76 string

---
+++ 76.cpp
@@ -0,0 +1,30 @@
+//
+//  76.cpp
+//  leetcode
+//
+//  Created by R Z on 2018/7/17.
+//  Copyright © 2018年 R Z. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+    }
+};
